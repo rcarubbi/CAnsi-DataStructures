90901e4d7b90232cc297211fd71c427cbab9ddd1
Raphael Carubbi Neto <rcarubbi@gmail.com>
2019-05-05 16:41:53 -0300

    Initial Commit

---
+++ ListaLinearSequencial/ListaLinearSequencial.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+#define true 1
+#define false 0
+typedef int bool;
+
+#define MAX 50
+
+typedef int TIPOCHAVE;
+
+typedef struct {
+    TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct {
+    REGISTRO A[MAX];
+    int nroElem;
+} LISTA;
+
+void inicializarLista(LISTA* l) {
+    l->nroElem = 0;
+}
+
+int tamanho(LISTA* l) {
+    return l->nroElem;
+}
+
+void exibirLista(LISTA* l) {
+    int i;
+    printf("Lista: \" ");
+    for(i=0;i<l->nroElem;i++){
+        printf("%i ", l->A[i].chave);
+    }
+    printf("\"\n");
+}
+
+int buscaSequencial(LISTA* l, TIPOCHAVE ch) {
+    int i = 0;
+    while(i < l->nroElem) {
+        if (ch==l->A[i].chave) {
+            return i;
+        } else {
+            i++;
+        }
+    }
+    return -1;
+}
+
+bool inserirElemLista(LISTA* l, REGISTRO reg, int i) {
+    int j;
+    if ((l->nroElem == MAX) || (i<0) || (i > l->nroElem)){
+        return false;
+    }
+    for(j = l->nroElem; j > i; j--) {
+        l->A[j] = l->A[j-1];
+    }
+    l->A[i] = reg;
+    l->nroElem++;
+    return true;
+}
+
+bool excluirElemLista(LISTA* l, TIPOCHAVE ch)
+{
+    int pos, j;
+    pos = buscaSequencial(l, ch);
+    if (pos == -1) return false;
+    for(j = pos; j < l->nroElem; j++) {
+        l->A[j] = l->A[j+1];
+    }
+    l->nroElem--;
+    return true;
+}
+
+void reinicializarLista(LISTA* l) {
+    l->nroElem = 0;
+}
+
+int main() {
+    LISTA lista;
+    inicializarLista(&lista);
+    exibirLista(&lista);
+    printf("Numero de elementos na lista: %i.\n",tamanho(&lista));
+    REGISTRO reg;
+    reg.chave = 9;
+    inserirElemLista(&lista,reg,0);
+    exibirLista(&lista);
+    reg.chave=3;
+    inserirElemLista(&lista,reg,1);
+    reg.chave=4;
+    inserirElemLista(&lista,reg,2);
+    reg.chave=1;
+    inserirElemLista(&lista,reg,3);
+    reg.chave=12;
+    inserirElemLista(&lista,reg,2);
+    exibirLista(&lista);
+    printf("Numero de elementos na lista: %i.\n",tamanho(&lista));
+ 
+    printf("Chave 4 encontrada na posicao: %i do arranjo A.\n",buscaSequencial(&lista,4));
+  
+    if (excluirElemLista(&lista,4)) printf("Exclusao bem sucedida: 4.\n");
+    if (excluirElemLista(&lista,8)) printf("Exclusao bem sucedida: 8.\n");
+    if (excluirElemLista(&lista,9)) printf("Exclusao bem sucedida: 9.\n");
+    exibirLista(&lista);
+    printf("Numero de elementos na lista: %i.\n",tamanho(&lista));
+ 
+    reinicializarLista(&lista);
+    exibirLista(&lista);
+    printf("Numero de elementos na lista: %i.\n",tamanho(&lista));
+ 
+    return 0;
+}
