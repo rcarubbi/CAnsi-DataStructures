88dbd5196b41649230df9e628fea79c25f52ed86
Raphael Carubbi Neto <rcarubbi@gmail.com>
2019-05-09 00:13:53 -0300

    Add Search, Insert and Delete methods

--- 11.CircularLinkedList/CircularLinkedList.c
+++ 11.CircularLinkedList/CircularLinkedList.c
@@ -66,9 +66,131 @@ NodePointer SentinelSearch(LinkedList* listPointer, Key id) {
     }
 }
 
+NodePointer SentinelOrderedSearch(LinkedList* listPointer, Key id) {
+    NodePointer currentNode = listPointer->Header->Next;
+    listPointer->Header->Item.Id = id;
+    
+    while(currentNode->Item.Id < id ) {
+        currentNode = currentNode->Next;
+    }
+
+    if (currentNode != listPointer->Header && currentNode->Item.Id == id) {
+        return currentNode;
+    } else {
+        return NULL;
+    }
+}
+
+NodePointer SequentialSearchWithPreviousByRef(LinkedList* listPointer, Key id, NodePointer* previousNode) {
+    *previousNode = listPointer->Header;
+    NodePointer currentNode = listPointer->Header->Next;
+
+    listPointer->Header->Item.Id = id;
+    while(currentNode->Item.Id < id) {
+        *previousNode = currentNode;
+        currentNode = currentNode->Next;
+    }
+
+    if (currentNode != listPointer->Header && currentNode->Item.Id == id) {
+        return currentNode;
+    } else {
+        return NULL;
+    }
+}
+
+bool InsertOrderedItem(LinkedList* listPointer, Item item) {
+    NodePointer previousNode, nodeToInsert;
+    nodeToInsert = SequentialSearchWithPreviousByRef(listPointer, item.Id, &previousNode);
+    
+    if (nodeToInsert != NULL) {
+        return false;
+    } 
+
+    nodeToInsert = (NodePointer) malloc(sizeof(Item));
+    nodeToInsert->Item = item;
+    nodeToInsert->Next = previousNode->Next;
+    previousNode->Next = nodeToInsert;
+}
+
+bool DeleteItem(LinkedList* listPointer, Key id) {
+    NodePointer previousNode, nodeToDelete;
+    nodeToDelete = SequentialSearchWithPreviousByRef(listPointer, id, &previousNode);
+    if (nodeToDelete == NULL){
+        return false;
+    } 
+
+    previousNode->Next = nodeToDelete->Next;
+    free(nodeToDelete);
+    return true;
+}
+
+void ReinitList(LinkedList* listPointer) {
+    NodePointer currentNode = listPointer->Header->Next;
+    while(currentNode != listPointer->Header) {
+        NodePointer nodeToDelete = currentNode;
+        currentNode = currentNode->Next;
+        free(nodeToDelete);
+    }
+
+    listPointer->Header->Next = listPointer->Header;
+}
+
 int main(void) {
     setlocale(LC_ALL, "");
     
+    LinkedList list;
+    Item item;
+    
+    InitList(&list);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 1;
+    InsertNode(&list, item);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 5;
+    InsertNode(&list, item);
+    ShowList(&list);
+     printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 12;
+    InsertNode(&list, item);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 9;
+    InsertNode(&list, item);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 22;
+    InsertNode(&list, item);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    item.Id = 14;
+    InsertNode(&list, item);
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+    
+    NodePointer node = SequentialOrderedSearch(&list, 12);
+    printf("12 esta no endereço %p\n", node);
+
+    if (DeleteItem(&list, 5)) printf("Item 5 excluído com sucesso\n");
+    printf("A lista possui %i elementos\n", Count(&list));
+   
+    if (DeleteItem(&list, 12)) printf("Item 12 excluído com sucesso\n");
+    printf("A lista possui %i elementos\n", Count(&list));
+
+    if (DeleteItem(&list, 6)) printf("Item 6 excluído com sucesso\n");
+    printf("A lista possui %i elementos\n", Count(&list));
+
+    ShowList(&list);
+    printf("A lista possui %i elementos\n", Count(&list));
+
+
     system("PAUSE > NUL");
     return 0;
 }
